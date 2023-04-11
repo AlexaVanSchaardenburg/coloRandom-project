3f97ceb2a590034ad8445fa9e5a32fae40e00dcf
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-11 14:37:58 -0600

    Update code to dispaly colors in boxes with hex code underneath

--- main.js
+++ main.js
@@ -3,25 +3,17 @@ var colorPalettes = [];
 
 //Variables
 var generateRandomButton = document.querySelector('#generate-random-button');
-var box1 = document.querySelector('#colored-box-1');
-var box2 = document.querySelector('#colored-box-2');
-var box3 = document.querySelector('#colored-box-3');
-var box4 = document.querySelector('#colored-box-4');
-var box5 = document.querySelector('#colored-box-5');
-var hexCode1 = document.querySelector('#color-1');
-var hexCode2 = document.querySelector('#color-2');
-var hexCode3 = document.querySelector('#color-3');
-var hexCode4 = document.querySelector('#color-4');
-var hexCode5 = document.querySelector('#color-5');
+var boxes = document.querySelectorAll('.box');
+var hexColors = document.querySelectorAll('.box p');
 
 //Event Listeners
 generateRandomButton.addEventListener('click', displayPalette);
 window.addEventListener('load', displayPalette);
 
-//Generating random hex codes 
+//Functions
 function generateRandomHex(){
     var hexCharacters = [];
-    for (var i=0; hexCharacters.length<7; i++){
+    for (var i=0; hexCharacters.length<6; i++){
       var characters = ['A', 'B', 'C', 'D', 'E', 'F',1,2,3,4,5,6,7,8,9]
       indexNum = Math.floor(Math.random() *14)
       hexCharacters.push(characters[indexNum])
@@ -30,36 +22,20 @@ function generateRandomHex(){
     return hexCode
 };
 
-//Generate random color palette function
 function generateRandomPalette() {
-    var colorPalette = []
-    for (var i=0; colorPalette.length<5; i++){
-        colorPalette.push(generateRandomHex())
-    }
-    colorPalettes.push(colorPalette)
-    return colorPalette
-};
+    var colorPalette = [];
+      for (var i=0; colorPalette.length<5; i++){
+          colorPalette.push(generateRandomHex())
+      }
+      colorPalettes.push(colorPalette)
+      return colorPalette
+  };
 
-//Change boxes to generated colors & change text below boxes to generated hex
 function displayPalette() {
-    var currentPalette = generateRandomPalette()
-    //display array in html
-    //display colors in boxes
-        //Declare 5 vars that are the boxes 
-        //use .style.background to set color to hex code
-    //display hex code numbers under boxes
-        //update the innertext of p elments by theuir id num (use query selectro to make them vars)
-    //color 1
-    console.log(box1)
-    hexCode1.innerText = `${currentPalette[0]}`
-    box1.style.background = currentPalette[0]
-    //color 2
-    hexCode2.innerText = `${currentPalette[1]}`
-    //color 3
-    hexCode3.innerText = `${currentPalette[2]}`
-    //color 4
-    hexCode4.innerText = `${currentPalette[3]}`
-    //color 5
-    hexCode5.innerText = `${currentPalette[4]}`
-};
+    var colorPalette = generateRandomPalette();
+    for (var i = 0; i < hexColors.length; i++) {
+        boxes[i].style.setProperty("background-color", colorPalette[i]);
+        hexColors[i].innerText = colorPalette[i];
+    }
+}
 
