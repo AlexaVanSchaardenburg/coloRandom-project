d5cf60bd07293a50f963e2f9b9f4116eac9859d0
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-10 19:37:20 -0600

    Delcare boxes and colors as variables in JS and psuedocode for displayPalette function

--- main.js
+++ main.js
@@ -2,7 +2,17 @@
 var colorPalettes = [];
 
 //Variables
-var generateRandomButton = document.querySelector('#generate-random-button')
+var generateRandomButton = document.querySelector('#generate-random-button');
+var box1 = document.querySelector('#colored-box-1');
+var box2 = document.querySelector('#colored-box-2');
+var box3 = document.querySelector('#colored-box-3');
+var box4 = document.querySelector('#colored-box-4');
+var box5 = document.querySelector('#colored-box-5');
+var hexCode1 = document.querySelector('#color-1');
+var hexCode2 = document.querySelector('#color-2');
+var hexCode3 = document.querySelector('#color-3');
+var hexCode4 = document.querySelector('#color-4');
+var hexCode5 = document.querySelector('#color-5');
 
 //Event Listeners
 generateRandomButton.addEventListener('click', generateRandomPalette);
@@ -33,6 +43,10 @@ function generateRandomPalette() {
 //Change boxes to generated colors & change text below boxes to generated hex
 function displayPalette() {
     //display array in html
-        //code displayed by changing the innerText of the 
+    //display colors in boxes
+        //Declare 5 vars that are the boxes 
+        //use .style.background to set color to hex code
+    //display hex code numbers under boxes
+        //update the innertext of p elments by theuir id num (use query selectro to make them vars)
 };
 
