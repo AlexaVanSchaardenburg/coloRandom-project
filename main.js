4af19c86502795f0e6e4c9f3dca4a8c3d0c5b622
Denzel Turambi <dturambi26@gmail.com>
2023-04-12 19:56:23 -0600

    Refactor savePalette and displaySaved functions to make them actually work

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ var savePaletteButton = document.querySelector('#save-palette-button');
 var boxes = document.querySelectorAll('.box');
 var hexColors = document.querySelectorAll('.item-wrapper p');
 var savedPalettesSection = document.querySelector('.saved-palettes-section');
-var savedDescription = document.querySelector('.saved-palettes');
+var savedColors = document.querySelector('.saved-palettes');
 
 //Event Listeners
 generateRandomButton.addEventListener('click', displayPalette);
 window.addEventListener('load', displayPalette);
 boxes.forEach(node => node.addEventListener('click', toggleLock));
-savePaletteButton.addEventListener('click', displaySaved);
+savePaletteButton.addEventListener('click', savePalette);
 
 //Functions
 function toggleLock(event) {
@@ -22,24 +22,24 @@ function toggleLock(event) {
       event.currentTarget.firstElementChild.classList.toggle('hidden');
       event.currentTarget.lastElementChild.classList.toggle('hidden');
     }
-}
+};
 
 function generateRandomHex(){
     var hexCharacters = [];
     for (var i=0; hexCharacters.length<6; i++){
-      var characters = ['A', 'B', 'C', 'D', 'E', 'F',1,2,3,4,5,6,7,8,9]
-      indexNum = Math.floor(Math.random() *14)
-      hexCharacters.push(characters[indexNum])
+      var characters = ['A','B','C','D','E','F',1,2,3,4,5,6,7,8,9];
+      indexNum = Math.floor(Math.random() *14);
+      hexCharacters.push(characters[indexNum]);
     }
-    hexCode = `#${hexCharacters.join('')}`
-    return hexCode
+    hexCode = `#${hexCharacters.join('')}`;
+    return hexCode;
 };
 
 function generateRandomPalette() {
     if (!currentPalette.length) {
         var colorPalette = [];
-        for (var i=0; colorPalette.length<5; i++){
-            colorPalette.push(generateRandomHex())
+        for (var i=1; colorPalette.length<5; i++){
+            colorPalette.push(generateRandomHex());
         }
         currentPalette = colorPalette;
         return colorPalette;
@@ -49,7 +49,7 @@ function generateRandomPalette() {
             var unlockIcon = document.getElementById(id);    
             var islocked = unlockIcon.classList.contains('hidden');
             if (!islocked) {
-                var newHex = generateRandomHex()
+                var newHex = generateRandomHex();
                 currentPalette.splice(i, 1, newHex);
             } 
         }
@@ -62,41 +62,31 @@ function displayPalette() {
         boxes[i].style.setProperty("background-color", currentPalette[i]);
         hexColors[i].innerText = currentPalette[i];
     }
-}
+};
 
-function displaySavedPalettes() {
-    savedPalettesSection.classList.remove('hidden');
-    if (!savedPalettes === []) {
-        savedDescription.classList.add('hidden');
-    }
-    savePalette();
- }; 
-
- function displaySaved() {
+function displaySaved() {
     savedColors.innerHTML = '';
-    if (savedPalettes === []){
-      savedColors.innerHTML = `<p class="saved-descriptor">No saved palettes yet!</p>`
-    } else {
       for (var i=0; i<savedPalettes.length; i++){
         savedColors.innerHTML += `<section class="color-palettes">
-                <article class="box", style="background-color: ${savedPalettes[i][0]}", id="colored-box-1">
+                <article class="box small-box", style="background-color: ${savedPalettes[i][0]}", id="colored-box-1">
                       </article>
-                <article class="box", style="background-color: ${savedPalettes[i][1]}", id="colored-box-2">
+                <article class="box small-box", style="background-color: ${savedPalettes[i][1]}", id="colored-box-2">
                       </article>
-                <article class="box", style="background-color: ${savedPalettes[i][2]}", id="colored-box-3">
+                <article class="box small-box", style="background-color: ${savedPalettes[i][2]}", id="colored-box-3">
                       </article>
-                <article class="box", style="background-color: ${savedPalettes[i][3]}", id="colored-box-4">
+                <article class="box small-box", style="background-color: ${savedPalettes[i][3]}", id="colored-box-4">
                       </article>
-                <article class="box", style="background-color: ${savedPalettes[i][4]}", id="colored-box-5">
+                <article class="box small-box", style="background-color: ${savedPalettes[i][4]}", id="colored-box-5">
                       </article>
               </section>`
         }
-    }
  };
 
 function savePalette() {
-    if (!savedPalettes.includes(currentPalette)) {
-            savedPalettes.push(currentPalette);
-            console.log(savedPalettes);
+    var paletteToPush = [];
+    for (var i=0; i<currentPalette.length; i++){
+        paletteToPush.push(currentPalette[i]);
     }
+    savedPalettes.push(paletteToPush);
+    displaySaved();
 };
