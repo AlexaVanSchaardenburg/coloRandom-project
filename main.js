2708c61c18077bb5b9bc611228253fa420dcacae
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-04-17 16:33:00 -0600

    Fix error on delete button

--- main.js
+++ main.js
@@ -31,17 +31,10 @@ savedPalettesSection.addEventListener('click', function (e) {
 savedPalettesSection.addEventListener('click', function(e) {
     if (e.target.classList.contains('small-box')) {
         var deleteButtonId = e.target.parentElement.lastElementChild.id;
-        console.log(deleteButtonId)
         var index = findIndex(deleteButtonId, savedPalettes);
-
-        //bug caused by some issue in how index is defined using the findIndex function - I think it has something to do with using the thedeleteButtonId
-        //bug happening because the code is trying to display the savedPalette everytime any area of that conatiner is clikced INCLUDING the X button - but when the x button is clicked the savedPalette is also deleted which causes the index in the dispaly the saved Palette funciton to return undefined becasue it doesn't exisit
-        //need to put the boxes that have the colors in them in their own div and target that div (creates one more level to bubble up from)
-
         var paletteHolder = [];
         for (var i=0; i<5; i++) {
             paletteHolder[i] = savedPalettes[index][i];
-            console.log()
         }
         currentPalette = paletteHolder;
 
@@ -49,23 +42,6 @@ savedPalettesSection.addEventListener('click', function(e) {
         displayPalette();
     }
 })
-
-// savedPalettesSection.addEventListener('click', function(e) {
-//     if (e.target.classList.contains('small-box')) {
-//         // var deleteButtonId = e.target.parentElement.lastElementChild.id;
-//         var index = findIndex(e.target.id, savedPalettes);
-
-//         var paletteHolder = [];
-//         for (var i=0; i<5; i++) {
-//             paletteHolder[i] = savedPalettes[index][i];
-//             console.log()
-//         }
-//         currentPalette = paletteHolder;
-
-//         resetToUnlock(boxes);
-//         displayPalette();
-//     }
-// });
 
 //Functions
 function findIndex(str, arr) {
@@ -140,19 +116,19 @@ function displaySaved() {
         savedColors.innerHTML = `No saved palettes yet!`
     } else {
         for (var i = 0; i < savedPalettes.length; i++) {
-            savedColors.innerHTML += `<section class="color-palettes">
-                    <div class="box small-box", style="background-color: ${savedPalettes[i][0]}", id="colored-box-1">
-                          </div>
-                    <div class="box small-box", style="background-color: ${savedPalettes[i][1]}", id="colored-box-2">
-                          </div>
-                    <div class="box small-box", style="background-color: ${savedPalettes[i][2]}", id="colored-box-3">
-                          </div>
-                    <div class="box small-box", style="background-color: ${savedPalettes[i][3]}", id="colored-box-4">
-                          </div>
-                    <div class="box small-box", style="background-color: ${savedPalettes[i][4]}", id="colored-box-5">
-                          </div>
-                    <img src="./assets/delete.png" class="small-box delete-button" id = ${savedPalettes[i]} alt="x shaped delete button">
-                  </section>`
+            savedColors.innerHTML += `<section class=color-palettes>
+                        <div class="box small-box", style="background-color: ${savedPalettes[i][0]}", id="colored-box-1">
+                            </div>
+                        <div class="box small-box", style="background-color: ${savedPalettes[i][1]}", id="colored-box-2">
+                            </div>
+                        <div class="box small-box", style="background-color: ${savedPalettes[i][2]}", id="colored-box-3">
+                            </div>
+                        <div class="box small-box", style="background-color: ${savedPalettes[i][3]}", id="colored-box-4">
+                            </div>
+                        <div class="box small-box", style="background-color: ${savedPalettes[i][4]}", id="colored-box-5">
+                            </div>
+                    <img src="./assets/delete.png" class="delete-button" id = ${savedPalettes[i]} alt="x shaped delete button">
+                </section>`
         }
     }
 };
